62821d436618eea4e1842b654e23050e15406172
vislyzhang <160227335+vislyzhang@users.noreply.github.com>
2024-03-15 14:51:28 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -29,9 +29,72 @@ size_t Size(void* ptr)
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
+void merge(int pData[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    
+    // Create temporary arrays
+    int *L = Alloc(sizeof(int) * n1);
+    int *R = Alloc(sizeof(int) * n2);
+    
+    // Copy data to temporary arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = pData[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = pData[m + 1 + j];
+    
+    // Merge the temporary arrays back into pData[l..r]
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            pData[k] = L[i];
+            i++;
+        }
+        else {
+            pData[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    // Copy the remaining elements of L[], if there are any
+    while (i < n1) {
+        pData[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    // Copy the remaining elements of R[], if there are any
+    while (j < n2) {
+        pData[k] = R[j];
+        j++;
+        k++;
+    }
+    
+    // Free the temporary arrays
+    DeAlloc(L);
+    DeAlloc(R);
+}
+
 void mergeSort(int pData[], int l, int r)
 {
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for large l and r
+        int m = l + (r - l) / 2;
+        
+        // Sort first and second halves
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+        
+        // Merge the sorted halves
+        merge(pData, l, m, r);
+    }
 }
+
 
 // parses input file to an integer array
 int parseData(char *inputFileName, int **ppData)
